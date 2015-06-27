afd7d3f6333a1e12a747790ea74429fb482509b8
Bianka L <bianka.lasocka@gmail.com>
2015-06-27 18:36:11 +0200

    cleaning

--- app/src/main/java/com/ingada/beaconhackaton/MainActivity.java
+++ app/src/main/java/com/ingada/beaconhackaton/MainActivity.java
@@ -1,190 +1,64 @@
 package com.ingada.beaconhackaton;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
+
+import android.content.Context;
 import android.content.Intent;
-import android.os.RemoteException;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-import com.kontakt.sdk.android.configuration.ForceScanConfiguration;
-import com.kontakt.sdk.android.configuration.MonitorPeriod;
-import com.kontakt.sdk.android.connection.OnServiceBoundListener;
-import com.kontakt.sdk.android.device.BeaconDevice;
-import com.kontakt.sdk.android.device.Region;
-import com.kontakt.sdk.android.factory.AdvertisingPackage;
-import com.kontakt.sdk.android.factory.Filters;
-import com.kontakt.sdk.android.manager.ActionController;
-import com.kontakt.sdk.android.manager.ActionManager;
-import com.kontakt.sdk.android.manager.BeaconManager;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
-    private static final int REQUEST_CODE_ENABLE_BLUETOOTH = 1;
 
-    private BeaconManager beaconManager;
-    private ActionController actionController;
+
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
 
     private Button visitor_button;
-    private Button company_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar ab = getActionBar();
-        ab.setSubtitle("Expo App");
+        visitor_button = (Button) findViewById(R.id.button_visitor);
+        visitor_button.setOnClickListener(new View.OnClickListener() {
 
-        //logn field mock
-
-        visitor_button = (Button) findViewById(R.id.visitor_button);
-        company_button = (Button) findViewById(R.id.company_button);
-
-        final Intent visitorIntent = new Intent(MainActivity.this, VisitorActivity.class);
-        final Intent companyIntent = new Intent(MainActivity.this, CompanyActivity.class);
-        visitor_button.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(visitorIntent);
-                                          }
-                });
-
-        company_button.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(companyIntent);
-                    }
-                });
-
-        beaconManager = BeaconManager.newInstance(this);
-        beaconManager.setMonitorPeriod(MonitorPeriod.MINIMAL);
-        beaconManager.setForceScanConfiguration(ForceScanConfiguration.DEFAULT);
-        beaconManager.addFilter(new Filters.CustomFilter() {
             @Override
-            public Boolean apply(AdvertisingPackage object) {
-                return object.getBeaconUniqueId().equals("wsn9");
+            public void onClick(View v)
+            {
+                Intent visitor_intent = new Intent(MainActivity.this, VisitorActivity.class);
+                startActivity(visitor_intent);
             }
         });
-        beaconManager.registerMonitoringListener(new BeaconManager.MonitoringListener() {
-            @Override
-            public void onMonitorStart() {
-
-                Log.d("Monitor","Start");
-
-            } // active scan period starts
-
-            @Override
-            public void onMonitorStop() {} // passive scan period starts
+        //Shared Preferences
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("name", "Elena");
+        editor.putInt("idName", 12);
+        editor.commit();
 
 
-            @Override
-            public void onBeaconAppeared(final Region region, final BeaconDevice beacon) {
+        final Intent visitorIntent = new Intent(MainActivity.this, VisitorActivity.class);
 
-                Log.d("Beacon", "Appeared");
-                actionController.notifyActionsFound(beacon);
-
-            } // beacon appeared within desired region for the first time
-
-            @Override
-            public void onBeaconsUpdated(final Region venue, final List<BeaconDevice> beacons) {
-
-                Log.d("Beacon","Updated");
-
-            } // beacons that are visible within specified region are provided through this method callback. This method has the same
-
-            @Override
-            public void onRegionEntered(final Region venue) {
-
-            } // Android device enters the Region for the first time
-
-            @Override
-            public void onRegionAbandoned(final Region venue) {
-
-            } // Android device abandons the region
-        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if(!beaconManager.isBluetoothEnabled()) {
-            final Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(intent, REQUEST_CODE_ENABLE_BLUETOOTH);
-        } else if(beaconManager.isConnected()) {
-            try {
-                beaconManager.startRanging();
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
 
-        } else {
-            connect();
-        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        beaconManager.stopMonitoring();
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        beaconManager.disconnect();
-        beaconManager = null;
+
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if(requestCode == REQUEST_CODE_ENABLE_BLUETOOTH) {
-            if(resultCode == Activity.RESULT_OK) {
-                connect();
-            } else {
-                Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_LONG).show();
-                getActionBar().setSubtitle("Bluetooth not enabled");
-            }
-            return;
-        }
-
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void connect() {
-        try {
-            beaconManager.connect(new OnServiceBoundListener() {
-                @Override
-                public void onServiceBound() {
-                    try {
-                        Set<Region> regions = new HashSet<Region>();
-
-                     /*   UUID id = UUID.fromString("f7826da6-4fa2-4e98-8024-bc5b71e0893e");
-                        Region ourBeacon = new Region (id,62609,54573);*/
-//
-//                        regions.add(ourBeacon);
-                        beaconManager.startMonitoring();
-                    } catch (RemoteException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        } catch (RemoteException e) {
-            throw new IllegalStateException(e);
-        }
-    }
 }
--- app/src/main/java/com/ingada/beaconhackaton/VisitorActivity.java
+++ app/src/main/java/com/ingada/beaconhackaton/VisitorActivity.java
@@ -1,43 +1,168 @@
 package com.ingada.beaconhackaton;
 
-import android.app.ActionBar;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.kontakt.sdk.android.configuration.ForceScanConfiguration;
+import com.kontakt.sdk.android.configuration.MonitorPeriod;
+import com.kontakt.sdk.android.connection.OnServiceBoundListener;
+import com.kontakt.sdk.android.device.BeaconDevice;
+import com.kontakt.sdk.android.device.Region;
+import com.kontakt.sdk.android.manager.BeaconManager;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 
 
 public class VisitorActivity extends ActionBarActivity {
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+    private static final int REQUEST_CODE_ENABLE_BLUETOOTH = 1;
+    private BeaconManager beaconManager;
+    private SharedPreferences sharedPreferences;
+    private TextView welcomeText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_visitor);
-        ActionBar ab = getActionBar();
-        ab.setTitle("Visitor");
+        setContentView(R.layout.activity_main);
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_visitor, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        welcomeText = (TextView) findViewById(R.id.welcome_text);
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String restoredText = prefs.getString("text", null);
+        if (restoredText != null) {
+            String name = prefs.getString("name", "No name defined");//"No name defined" is the default value.
+            int idName = prefs.getInt("idName", 0); //0 is the default value.
+            welcomeText.setText(name);
         }
 
-        return super.onOptionsItemSelected(item);
+
+
+
+        beaconManager = BeaconManager.newInstance(this);
+        beaconManager.setMonitorPeriod(MonitorPeriod.MINIMAL);
+        beaconManager.setForceScanConfiguration(ForceScanConfiguration.DEFAULT);
+//        beaconManager.addFilter(new Filters.CustomFilter() {
+//            @Override
+//            public Boolean apply(AdvertisingPackage object) {
+//                return object.getBeaconUniqueId().equals("wsn9");
+//            }
+//        });
+        beaconManager.registerMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onMonitorStart() {
+
+                Log.d("Monitor","Start");
+
+            } // active scan period starts
+
+            @Override
+            public void onMonitorStop() {} // passive scan period starts
+
+
+            @Override
+            public void onBeaconAppeared(final Region region, final BeaconDevice beacon) {
+
+                Log.d("Beacon", "Appeared");
+
+            } // beacon appeared within desired region for the first time
+
+            @Override
+            public void onBeaconsUpdated(final Region venue, final List<BeaconDevice> beacons) {
+
+                Log.d("Beacon","Updated");
+
+            } // beacons that are visible within specified region are provided through this method callback. This method has the same
+
+            @Override
+            public void onRegionEntered(final Region venue) {
+
+            } // Android device enters the Region for the first time
+
+            @Override
+            public void onRegionAbandoned(final Region venue) {
+
+            } // Android device abandons the region
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!beaconManager.isBluetoothEnabled()) {
+            final Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, REQUEST_CODE_ENABLE_BLUETOOTH);
+        } else if(beaconManager.isConnected()) {
+            try {
+                beaconManager.startRanging();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        beaconManager.stopMonitoring();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.disconnect();
+        beaconManager = null;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CODE_ENABLE_BLUETOOTH) {
+            if(resultCode == Activity.RESULT_OK) {
+                connect();
+            } else {
+                Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_LONG).show();
+                getActionBar().setSubtitle("Bluetooth not enabled");
+            }
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void connect() {
+        try {
+            beaconManager.connect(new OnServiceBoundListener() {
+                @Override
+                public void onServiceBound() {
+                    try {
+                        Set<Region> regions = new HashSet<Region>();
+
+                     /*   UUID id = UUID.fromString("f7826da6-4fa2-4e98-8024-bc5b71e0893e");
+                        Region ourBeacon = new Region (id,62609,54573);*/
+//
+//                        regions.add(ourBeacon);
+                        beaconManager.startMonitoring();
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (RemoteException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
