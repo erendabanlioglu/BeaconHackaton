01180205a8b206f536783d39efc3b94e28c70552
Bianka L <bianka.lasocka@gmail.com>
2015-06-27 23:39:34 +0200

    click on list to different activity

---
+++ app/src/main/java/com/ingada/beaconhackaton/OfferDetailActivity.java
@@ -0,0 +1,48 @@
+package com.ingada.beaconhackaton;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class OfferDetailActivity extends ActionBarActivity {
+
+    TextView welcometext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_detail);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_offer_detail, menu);
+        welcometext = (TextView) findViewById(R.id.welcome_text_offer);
+        Intent intent = getIntent();
+    intent.getStringExtra("tag");
+        welcometext.setText(intent.getStringExtra("jobTitle")+"/n"+intent.getStringExtra("jobDesc"));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
