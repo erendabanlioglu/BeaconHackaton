d997bb182df54553aaadd5ffb70985df29a4f856
Bianka L <bianka.lasocka@gmail.com>
2015-06-27 18:39:57 +0200

    log

--- app/src/main/java/com/ingada/beaconhackaton/MainActivity.java
+++ app/src/main/java/com/ingada/beaconhackaton/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,6 +31,7 @@ public class MainActivity extends ActionBarActivity {
             {
                 Intent visitor_intent = new Intent(MainActivity.this, VisitorActivity.class);
                 startActivity(visitor_intent);
+                Log.d("login", "login");
             }
         });
         //Shared Preferences
